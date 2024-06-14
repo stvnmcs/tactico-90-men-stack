03184f26478c23ca458fe73d2ffa30a07533d4da
StevenMacias <stevenmcs22@gmail.com>
2024-06-14 11:43:46 -0400

    thank you jordan

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
+const session = require('express-session');
 
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
@@ -9,19 +10,33 @@ const morgan = require("morgan");
 
 const port = process.env.PORT ? process.env.PORT : "3000";
 
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+);
+
+
+const authController = require("./controllers/auth.js");
+app.use("/auth", authController);
+
+
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
-app.use(morgan('dev'));
-
 
 app.get("/" , (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+        user: req.session.user,
+    });
 });
 
 app.listen(port, () => {
